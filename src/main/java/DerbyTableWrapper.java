ad891b374b70ea29d63af790537353ad942349cd
Sarah <MistaMishMash@users.noreply.github.com>
2019-09-16 19:32:42 +1000

    first commit.

---
+++ src/main/java/DerbyTableWrapper.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * Provides the functionality of Derby database
+ * Will offer the following functionality:
+ * -    Create table
+ * -    Create
+ * -    Retrieve
+ * -    Update
+ * -    Delete (x) not needed
+ *
+ * @author MistaMishMash
+ *
+ * **/
+
+public class DerbyTableWrapper {
+
+
+
+}
