b7776679614e936b7423688df48d6252c0209d67
Sarah <MistaMishMash@users.noreply.github.com>
2019-10-09 18:01:54 +1100

    updated private field names in HomeHotbar.

--- src/main/java/InterfaceObjects/ReturnHomeHotbar.java
+++ src/main/java/InterfaceObjects/ReturnHomeHotbar.java
@@ -7,34 +7,34 @@ import java.awt.event.ActionListener;
 
 public class ReturnHomeHotbar extends JPanel implements ActionListener {
 
-    private JButton backButton;
-    private ReturnListener backListener;
+    private JButton homeButton;
+    private ReturnListener homeButtonListener;
 
     public ReturnHomeHotbar() {
         //button name can be changed here
-        backButton = new JButton("Return to Main Menu");
+        homeButton = new JButton("Return to Main Menu");
 
         // what the button will do when you click it
-        backButton.addActionListener(this);
+        homeButton.addActionListener(this);
 
         // button appearance
-        backButton.setBackground(Color.darkGray);
-        backButton.setBorderPainted(true);
-        backButton.setFocusPainted(false); // stops that outline appearing when the option has been selected before
-        backButton.setForeground(Color.white);
+        homeButton.setBackground(Color.darkGray);
+        homeButton.setBorderPainted(true);
+        homeButton.setFocusPainted(false); // stops that outline appearing when the option has been selected before
+        homeButton.setForeground(Color.white);
 
 
         // button layout (align left)
         setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        add(backButton);
+        add(homeButton);
 
     }
 
     //setter for the listener, this is how the controller accesses the button.
-    public void setReturnListener(ReturnListener listener) {this.backListener = listener; }
+    public void setReturnListener(ReturnListener listener) {this.homeButtonListener = listener; }
 
     public void actionPerformed(ActionEvent event) {
-        backListener.returnClicked();
+        homeButtonListener.returnClicked();
     }
 }
